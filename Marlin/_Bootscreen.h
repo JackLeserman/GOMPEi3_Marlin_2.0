c0d9dc0dd79d7aaad10c40b314e7289e43fedc87
JackLeserman <39784006+JackLeserman@users.noreply.github.com>
2021-05-11 12:37:20 -0400

    GOMPEI_3
    Test

---
+++ Marlin/_Bootscreen.h
@@ -0,0 +1,13 @@
+/**
+ * Made with Marlin Bitmap Converter
+ * https://marlinfw.org/tools/u8glib/converter.html
+ *
+ * This bitmap from the file 'boot-text-c201-test.bmp'
+ */
+#pragma once
+
+#define CUSTOM_BOOTSCREEN_BMPWIDTH  128
+
+const unsigned char custom_start_bmp[] PROGMEM = {
+  
+};
